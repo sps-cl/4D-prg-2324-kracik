d8ffd70ac5cc7ca08daa2cc47b91762252c13a61
sebastiankracik <sebvonopava@gmail.com>
2023-09-18 15:10:07 +0200

    Added: Library

---
+++ LibraryApp/LibraryApp/models/DVD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.models
+{
+    internal class DVD : LibraryItem<DVD>
+    {
+        public int LenghtOfTrack { get; }
+        public string Author { get; }
+
+        public DVD(int LenghtOfTrack, string Author, int id, string Title) : base(id, Title)
+        {
+            this.LenghtOfTrack = LenghtOfTrack;
+            this.Author = Author;
+        }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Autor {Author} a délka skladby: {LenghtOfTrack}");
+        }
+    }
+}
---
+++ LibraryApp/LibraryApp/models/LibraryItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.models
+{
+    internal abstract class LibraryItem<T>
+    {
+        private int ID { get; }
+        private string Title { get; }
+        public bool IsAvaiable { get; set; }
+
+        public LibraryItem(int id, string Title) 
+        {
+            this.ID = id;
+            this.Title = Title;
+            this.IsAvaiable = true;
+        }
+        public abstract void DisplayInfo();
+    }
+}
