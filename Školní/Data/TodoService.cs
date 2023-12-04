4689d07bd06b3078eb1c08a686052ac878e188df
sebastiankracik <sebvonopava@gmail.com>
2023-12-04 13:55:29 +0100

    Update

---
+++ Školní/Data/TodoService.cs
@@ -0,0 +1,30 @@
+﻿using Školní.Models;
+
+namespace Školní.Data
+{
+    private class ITodoService
+    {
+        List<Todo> _Todo = new List<Todo>();
+
+        public async Task<bool> AddTodo(ToDo toDo)
+        {
+            toDo.Id = Guid.NewGuid();
+
+            toDo.CreatedData = DateTime.Now;
+
+            //v reálném světě by probehlo ulozeni do DB
+            _Todo.Add(toDo);
+
+            return true;
+        }
+
+        public async Task<List<ToDo>> GetTodoAll()
+        {
+           
+            return _Todo.OrderBy(x => x.CreatedData).ToList();
+        }
+
+
+
+    }
+}
---
+++ Školní/Models/ToDo.cs
@@ -0,0 +1,12 @@
+﻿namespace Školní.Models
+{
+    public class ToDo
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedData { get; set; }
+
+
+    }
+}
